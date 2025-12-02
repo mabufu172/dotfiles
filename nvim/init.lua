36f8e41ff4c3f462cc47824488c12a60ca8e9bcf
mabufu172 <reidev241@gmail.com>
2025-12-02 17:33:20 +0700

    Initial commit

---
+++ nvim/init.lua
@@ -0,0 +1,8 @@
+vim.opt.cursorline = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.mouse = ""
+vim.opt.scrolloff = 5
+vim.api.nvim_set_hl(0, "Normal", { bg = "#252524" })
